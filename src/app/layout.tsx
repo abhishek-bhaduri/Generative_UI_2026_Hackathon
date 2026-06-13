71c1620a2b8b1ad9069ba01df4e355835012f04c
Abhishek Bhaduri <bhaduri.abhishek@gmail.com>
2026-06-13 16:24:27 +0100

    fix: make root route open RFP demo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${jakarta.variable} ${splineMono.variable}`}>
       <head>
-        <title>CopilotKit</title>
+        <title>RFP Intake Cockpit</title>
         <link
           rel="icon"
           type="image/svg+xml"
